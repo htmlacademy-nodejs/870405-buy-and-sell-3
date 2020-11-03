2fd5b9cddf3cf8160f2d399692f607f1fe9216c4
Igor Ilyaguev <non_2002@mail.ru>
2020-11-03 14:49:44 +0300

    fix service.js to complete module1-task3

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+
+const DEFAULT_COMMAND = `--help`;
+const OFFERS_MAX_COUNT = 1000;
+const [userCommand, commandParam] = process.argv.slice(2);
+const programExitCode = {
+  success: 1,
+  error: 0,
+};
+
+if (!Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(programExitCode.success);
+}
+
+if (parseInt(commandParam, 10) > OFFERS_MAX_COUNT) {
+  console.info(`Не больше 1000 объявлений`);
+  process.exit(programExitCode.error);
+}
+
+Cli[userCommand].run(commandParam);
